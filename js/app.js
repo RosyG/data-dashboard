8649b4cf1d0b6ef879753e06e25013e5869218fa
RosyG <rmga2106@gmail.com>
2017-12-18 19:19:12 -0600

    añadiendo gráficas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
-
 var selection=document.getElementById("selection");//Por medio de DOM guardo en una variable las opciones de la sede-generación que puede seleccionar del user.
 selection.addEventListener("change", generationSelection);//Creando el evento cambio, para detectar lo que el user selecciona.
 
@@ -48,8 +47,29 @@ function averageTeachers (allRatings) {
     var ratingsTeacher = allRatings[i]["teacher"];//Accediendo a la calificación de un sprint.
     averageTeacher = ratingsTeacher + averageTeacher;//Suma de la calificación a lxs Teachers en cada sprint.
   }
-  averageTeacher = (averageTeacher / allRatings.length).toFixed(2);
+  averageTeacher = parseInt((averageTeacher / allRatings.length).toFixed(2));
   //console.log(averageTeacher);//Muestra el promedio de la calificación de los Teaches en todos los sprints.
+
+  //Gráfica de las puntuaciones de los teachers en promedio.
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawCharTeacher);
+function drawCharTeacher() {
+  var data = google.visualization.arrayToDataTable([
+    ['Teacher', 'PromedioTeacher', { role: 'style' }],
+    ['Teacher', averageTeacher, 'stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2']
+
+  ]);
+
+  var options = {
+      title: 'Puntuación promedio de l@s profesores',
+          /*slices: {
+           0: { color: 'yellow' },
+           /*1: { color: 'purple' }*/
+       }
+  var chart = new google.visualization.ColumnChart(document.getElementById("teacher"));
+  chart.draw(data, options);
+}//Fin de la función drawCharTeacher.
+
 }//Fin de la función averageTeachers ().
 
 function averageJedi (allRatings) {
@@ -58,8 +78,26 @@ function averageJedi (allRatings) {
     var ratingsJedi = allRatings[i]["jedi"];//Accediendo a la calificación de un sprint.
     averageJedi = (ratingsJedi + averageJedi);//Suma de la calificación a lxs Jedi Master en cada sprint.
   }
-  averageJedi = (averageJedi / allRatings.length).toFixed(2);
+  averageJedi = parseInt((averageJedi / allRatings.length).toFixed(2));
   //console.log(averageJedi);//Muestra el promedio de la calificación de los Jedi Master en todos los sprints.
+
+  //Gráfica de las puntuaciones de los Jedi en promedio.
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawCharJedi);
+function drawCharJedi() {
+  var data = google.visualization.arrayToDataTable([
+    ['Jedi', 'PromedioJedi', { role: 'style' }],
+    ['Jedi', averageJedi, 'stroke-color: #459A81; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2']
+
+  ]);
+
+  var options = {
+      title: 'Puntuación promedio de l@s profesores',
+       }
+  var chart = new google.visualization.ColumnChart(document.getElementById("jedi"));
+  chart.draw(data, options);
+}//Fin de la función drawCharJedi.
+
 }//Fin de la función averageJedi ().
 
 
@@ -71,8 +109,33 @@ function satisfiedStudents (allRatings) {
     var unsatisfied = allRatings[i]["student"]["no-cumple"];//Porcentaje de estudiantes NO satisfechas.
     percentageUnsatisfied = percentageUnsatisfied + unsatisfied;//Suma de la insatisfacción en cada SPRINT.
   }
-  var percentageSatisfied = (100 - (percentageUnsatisfied / allRatings.length)).toFixed(2); //Porcentaje de la satisfacción promedio en cada SPRINT
-  //console.log(percentageSatisfied + "%");//Muestra el porcentaje de SATISFACIÓN en promedio de todos los SPRINTS cursados.
+  percentageUnsatisfied = (percentageUnsatisfied / allRatings.length).toFixed(2)
+  var percentageSatisfied = 100 - percentageUnsatisfied ; //Porcentaje de la satisfacción promedio en cada SPRINT
+  //console.log(percentageSatisfied);//Muestra el porcentaje de SATISFACIÓN en promedio de todos los SPRINTS cursados.
+
+  //Gráfica de la satisfación de las estudiantes.
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChartSatisfiedGeneral);
+  function drawChartSatisfiedGeneral() {
+    var data = google.visualization.arrayToDataTable([
+      ['aprobación', 'promedio por sprint'],
+      ['Satisfechas', percentageSatisfied],
+      ['Insatisfechas', parseInt(percentageUnsatisfied)],
+    ]);
+    var options = {
+      title: 'Porcentaje General de las estudiantes satisfechas por su experiencia en Laboratoria',
+      pieHole: 0.4,
+      slices: {
+       0: { color: 'yellow' },
+       1: { color: 'purple' }
+     },
+   }
+
+    var chart = new google.visualization.PieChart(document.getElementById('satisfied'));
+    chart.draw(data, options);
+  } //Fin de la función drawChartSatisfiedGeneral.
+
+
 
 }//Fin de la función satisfiedStudents ().
 
@@ -108,8 +171,8 @@ function NetPromoterScore (allRatings) {
 
 
 function exceedsGoal(allTheStudents) {
-  var maxTech = 1650;//puntos máximos de TECH.
-  var maxHse = 1000;//puntos máximos de HSE.
+  var maxTech = 1800;//puntos máximos de TECH.
+  var maxHse = 1200;//puntos máximos de HSE.
   var pointsTecH = [];
   var pointsHse = [];
   var sprint = [];
@@ -149,19 +212,54 @@ function exceedsGoal(allTheStudents) {
         approvedHse = approvedHse + 1;//# de alumnas que supera el 70% en HSE.
     }
     //console.log(approvedStudent);//Cantidad de estudiantes que supera la meta.
-    console.log(allTheStudents.length);//Cantidad de estudiantes en la sede y generación.
+    var totalStudents = parseInt(allTheStudents.length);
+    console.log(totalStudents + "total de estudiantes");//Cantidad de estudiantes en la sede y generación.
   }
 
-  var percentageApproved = ((approvedStudent / allTheStudents.length) * 100).toFixed(2);//Procentaje que reprenta en comparación al total de estudiantes.
-  var percentageApprovedTech = ((approvedTech / allTheStudents.length) * 100).toFixed(2);//Porcentaje de alumnas que supera el mínimo solo en TECH.
-  var percentageApprovedHse = ((approvedHse / allTheStudents.length) * 100).toFixed(2);//Porcentaje de alumnas que supera el mínimo solo en HSE.
+  var percentageApproved = parseInt(((approvedStudent / allTheStudents.length) * 100).toFixed(2));//Procentaje que reprenta en comparación al total de estudiantes.
+  var percentageApprovedTech = parseInt(((approvedTech / allTheStudents.length) * 100).toFixed(2));//Porcentaje de alumnas que supera el mínimo solo en TECH.
+  var percentageApprovedHse = parseInt(((approvedHse / allTheStudents.length) * 100).toFixed(2));//Porcentaje de alumnas que supera el mínimo solo en HSE.
   var disapproved = (100 - percentageApproved).toFixed(2);
-
-  /*console.log("tech"+ percentageApprovedTech + "%");
+/*
+    console.log("tech"+ percentageApprovedTech + "%");
     console.log("hse" + percentageApprovedHse + "%");
-    console.log(percentageApproved + "%");//Muestra el Procentaje de alumnas que aprobaron el 70% en HSE Y TECH.
-    console.log(disapproved + "%");//Muestra el porcentaje de alumans que NO aprobaron el 70% en HSE y TECH.
-  */
+    console.log("ambos"+percentageApproved + "%");//Muestra el Procentaje de alumnas que aprobaron el 70% en HSE Y TECH.
+    console.log("ambos"+disapproved + "%");//Muestra el porcentaje de alumans que NO aprobaron el 70% en HSE y TECH.
+*/
+  //Gráfica de las chicas que superan el %70 en promedio por sprint.
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChartTech);
+  google.charts.setOnLoadCallback(drawChartHse);
+  function drawChartTech() {
+    var data = google.visualization.arrayToDataTable([
+      ['tech', 'promedio por sprint'],
+      ['Mayor al 70%', percentageApprovedTech],
+      ['Menor al 70%', totalStudents],
+    ]);
+    var options = {
+      title: 'Porcentaje General de las estudiantes calificación mayor al 70% en TECH',
+      pieHole: 0.4,
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('tech'));
+    chart.draw(data, options);
+  } //Fin de la función drawChartTech.
+
+
+  function drawChartHse() {
+    var data = google.visualization.arrayToDataTable([
+      ['aprobación', 'promedio por sprint'],
+      ['Satisfechas', percentageApprovedHse],
+      ['Insatisfechas', totalStudents],
+    ]);
+    var options = {
+      title: 'Porcentaje General de las estudiantes calificación mayor al 70% en HSE',
+      pieHole: 0.4,
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('hse'));
+    chart.draw(data, options);
+  } //Fin de la función drawChartHse.
+
+
 }//Fin de exceedsGoal ()
 
 
@@ -183,4 +281,23 @@ function desertion (allTheStudents) {
   console.log(percentageDesertion + "%");//Porcentaje de deserción.
   console.log(percentageActive + "%");//Porcentaje de alumnas activas.
   */
+  //Gráfica de la deserción de estudiantes
+    google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChartDesertionGeneral);
+  function drawChartDesertionGeneral() {
+    var data = google.visualization.arrayToDataTable([
+      ['estado de las estudiantes', 'promedio por sprint'],
+      ['Activas', percentageActive],
+      ['Deserción', parseInt(percentageDesertion)],
+    ]);
+
+    var options = {
+      title: 'Porcentaje General de la deserción en Laboratoria',
+      pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('desertion'));
+    chart.draw(data, options);
+  } //Fin de la función drawChartDesertionGeneral.
+
 }//Fin de desertion ()
